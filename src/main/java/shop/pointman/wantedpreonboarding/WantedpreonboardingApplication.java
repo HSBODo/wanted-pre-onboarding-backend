4e910fc7801d8cd8dff2e220afc904cb641c4ad3
HSBODo <vinsulill@gmaile.com>
2023-08-02 20:55:08 +0900

    Initial commit

---
+++ src/main/java/shop/pointman/wantedpreonboarding/WantedpreonboardingApplication.java
@@ -0,0 +1,13 @@
+package shop.pointman.wantedpreonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedpreonboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedpreonboardingApplication.class, args);
+	}
+
+}
